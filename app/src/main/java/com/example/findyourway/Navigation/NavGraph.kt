f561e2576d9d6308cf2bdab0a2cba91df1861c24
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-02-25 13:13:51 +0530

    Added a Simple splash Screen

--- app/src/main/java/com/example/findyourway/Navigation/NavGraph.kt
+++ app/src/main/java/com/example/findyourway/Navigation/NavGraph.kt
@@ -6,18 +6,22 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.findyourway.Login.LoginPage
 import com.example.findyourway.Login.NewAccountPage
+import com.example.findyourway.Splash.SplashScreen
 
 @Composable
 fun NavGraph(navHostController: NavHostController) {
     NavHost(
         navController = navHostController,
-        startDestination = Screen.LoginPage.route
+        startDestination = Screen.SplashScreen.route
     ){
         composable(route = Screen.LoginPage.route){
             LoginPage(navHostController = navHostController)
         }
         composable(route = Screen.NewAccountPage.route){
             NewAccountPage(navHostController = navHostController)
+        }
+        composable(route = Screen.SplashScreen.route){
+            SplashScreen(navHostController = navHostController)
         }
     }
 }
--- app/src/main/java/com/example/findyourway/Navigation/Screen.kt
+++ app/src/main/java/com/example/findyourway/Navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.example.findyourway.Navigation
 sealed class Screen(val route: String) {
     object LoginPage: Screen(route = "loginPage")
     object NewAccountPage: Screen(route = "newAccountPage")
+    object SplashScreen: Screen(route = "SplashScreen")
 }
